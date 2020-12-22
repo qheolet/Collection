c1ac0341c916052bcfba188a53b93e4ebb5e8960
qheolet <qheolet@gmail.com>
2020-12-21 21:57:47 -0500

    1 file modified

--- library.ts
+++ library.ts
@@ -1,106 +1,84 @@
-import { of, Observable, BehaviorSubject } from 'rxjs'; 
+import { of, Observable, BehaviorSubject } from "rxjs";
 
-
-interface Options{
-  id: string;
+interface Options {
+  id?: string;
 }
 
-export class Collection<T>{
-  private _list : BehaviorSubject<Array<T>> = new BehaviorSubject([]);
-  private _id : string;
+export class Collection<T> {
+  private _list: T[] = [];
+  private _id: string;
 
-  constructor (item: Array<T>, options?: Options) {
+  constructor(item: Array<T>, options?: Options) {
     if (options) {
       this._id = options.id;
     }
-    
+
     this.set(item);
-  } 
-
-  add(item: T): number{
-    const r = this.snapshot
-    r.push(item);
-    this._list.next(r);
-    return this.snapshot.length;
   }
 
-  getItemByIndex(idx: number):T {
-    return this.snapshot[0];
+  set(item: Array<T>): number {
+    this._list.concat(item);
+    return this._list.length;
   }
 
-  set(item:Array<T>): number{
-    this._list.next(this.snapshot.concat(item))
-    return this.snapshot.length;
+  add(item: T): number {
+    this._list.push(item);
+    return this._list.length;
   }
 
-  getById(id:string){
-    if (!this._id) {
-      throw new Error('Need to set the id attr');
-    }
-    const r = this.snapshot;
-    const find =  r.filter((e)=>{
-      return e[this._id]===id;
-    });
-
-    return find.length? find[0] : undefined;
+  getItemByIndex(idx: number): T {
+    return this._list[idx];
   }
 
-  reverse(): BehaviorSubject<Array<T>> {}
-  sort(property:string,order: 'asc'|'dsc'): T{}
-  query(fn: (a:T,b:T)=>number):T[]{
-    return this.snapshot.sort(fn);
+  reverse(): T[] {
+    return this._list.reverse();
   }
-  delete(idx: number): T{}
-  pop(): T{}
-  switch(): boolean{}
-  
-  
-  indexBy(fn: Function, init={}): {[key:string] : T[]} {
+
+  query(sortFn: (a: T, b: T) => number): T[] {
+    return this._list.sort(sortFn);
+  }
+
+  deleteByIdx(idx: number): T[] { 
+    const h = this._list.splice(idx, 1);
+    return this._list;
+  }
+
+  pop(): T {
+    return this._list.pop();
+  }
+
+  indexBy(fn: Function, init = {}): { [key: string]: T[] } {
     const r = init;
-    const list = this.snapshot;
+    const list = this._list;
 
-    list.forEach((item) => {
-      if ( r.hasOwnProperty(fn(item))) {
+    list.forEach(item => {
+      if (r.hasOwnProperty(fn(item))) {
         r[fn(item)].push(item);
       } else {
         r[fn(item)] = [item];
       }
     });
-    
+
     return r;
   }
 
-  getObservable(){
-    return this._list;
+  get size() {
+    return this._list.length;
   }
-
-
-  get size(){
-    return this.snapshot.length;
-  }
-
-  get snapshot(){
-    return this._list.getValue();
-  }
-
 }
-export class Model{
 
-  has(attr: string){
+export class Model {
+  has(attr: string) {
     return this.hasOwnProperty(attr);
   }
 
-  get(attr:string): Error | any{
-    if(!this.has(attr)) {
-     throw new Error('Attribute does not available');
+  get(attr: string): Error | any {
+    if (!this.has(attr)) {
+      throw new Error("Attribute does not available");
     }
     return this[attr];
   }
 
-  
-  beforeSet(){}
-  afterSet(){}
-
-
-
+  beforeSet() {}
+  afterSet() {}
 }
